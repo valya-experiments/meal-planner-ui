be52e16da68047501e40d03acb1407498f652c7e
Valentin Berlin <valentin.berlin@protonmail.com>
2021-09-01 01:42:12 +0200

    add alias for utils/tests
    * tsconfig
    * jest.config

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   resetMocks: true,
-  // transform: {
-  //   "^.+\\.(ts|tsx)?$": "ts-jest",
-  // },
   testMatch: ["**/*.(test|spec).(ts|tsx)"],
   coveragePathIgnorePatterns: ["/node_modules/"],
   setupFilesAfterEnv: ["./utils/tests/setupTestingEnv.ts"],
@@ -13,5 +10,6 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/utils/tests/fileMock.ts",
     "\\.(css|less|scss)$": "<rootDir>/utils/tests/fileMock.ts",
+    "^utils/(.*)": "<rootDir>/utils/$1",
   },
 };
--- src/views/Meal.spec.tsx
+++ src/views/Meal.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "../../utils/tests";
+import { render, screen } from "utils/tests";
 import { MealView } from "./Meal";
 
 test("works with RTL", async () => {
