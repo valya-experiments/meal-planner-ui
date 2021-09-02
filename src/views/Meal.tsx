5728f2ce987153ec5d9cda307dd35ad61be6ef28
Valentin Berlin <valentin.berlin@protonmail.com>
2021-09-02 08:45:04 +0200

    add TailwindCSS

--- src/views/Meal.tsx
+++ src/views/Meal.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const MealView = () => {
   return (
-    <div className="home">
+    <div className="home px-4 py-2 bg-green-200 text-red-500 rounded">
       <h1>Meal page</h1>
     </div>
   );
