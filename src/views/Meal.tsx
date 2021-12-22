b7a2f877e4a9283f897de52b6f59860b8d735b67
Valentin Berlin <valentin.berlin@protonmail.com>
2021-12-22 23:30:06 +0100

    add prettier autoformatter

--- src/views/Meal.tsx
+++ src/views/Meal.tsx
@@ -4,17 +4,17 @@ import { DatabaseError, DatabaseResponse, Meal } from "src/model";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const MealView = () => {
-  const { isLoading, error, data: resData } = useQuery<
-    DatabaseResponse<Meal[]>,
-    DatabaseError
-  >(
+  const {
+    isLoading,
+    error,
+    data: resData,
+  } = useQuery<DatabaseResponse<Meal[]>, DatabaseError>(
     "mealsList",
     async (): Promise<DatabaseResponse<Meal[]>> => {
       const res = await fetch("/api/meals/");
       return await res.json();
     }
   );
-
   return (
     <div className="px-4 py-2 text-red-500 bg-green-200 rounded home">
       <h1>Meal page</h1>
